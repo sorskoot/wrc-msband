cfeb58ad7a3dc7b51792d483444a098173ef87d0
Sorskoot <mental_c@hotmail.com>
2015-07-31 00:39:37 +0200

    Initial Commit

---
+++ Sample/msband-sample/js/default.js
@@ -0,0 +1,54 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=329104
+(function () {
+    "use strict";
+    window.$ = function (query) { return document.querySelector(query); }
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    var accelx, accely, accelz;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                accelx = $("#accelX");
+                accely = $("#accelY");
+                accelz = $("#accelZ");
+
+                var band = new TimmyTools.Band();
+                band.onaccelerometerchanged = function (sensordata) {
+                    accelx.innerText = sensordata.z;
+                    accely.innerText = sensordata.y;
+                    accelz.innerText = sensordata.z;
+                };
+
+                band.getPaired().then(function (bands) {
+                    var x = bands;
+                    band.connect(bands[0].name).then(function (y) {
+
+                    }, function (err) {
+                        console.log(err);
+                    })
+
+                });
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application was suspended and then terminated.
+                // To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.start();
+})();
---
+++ Winmd-Band/Band.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Band;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using System.Threading.Tasks;
+
+namespace TimmyTools
+{
+    public sealed class BandInfo
+    {
+        public int ConnectionType { get; set; }
+        public string Name { get; set; }
+    }
+    public sealed class SensorReading
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+    }
+
+    public sealed class Band : IDisposable
+    {
+        IBandClient connection;
+
+        public event EventHandler<object> AccelerometerChanged;
+        public event EventHandler<object> StatusChanged;
+
+        /// <summary>
+        /// Get a list of bands that are currently paired
+        /// </summary>
+        /// <returns>a list of bands that are currently paired</returns>
+        public IAsyncOperation<IList<BandInfo>> GetPaired()
+        {
+            return AsyncInfo.Run<IList<BandInfo>>((token) =>
+
+                 Task.Run<IList<BandInfo>>(async () =>
+                {
+                    IBandInfo[] bandInfo = await BandClientManager.Instance.GetBandsAsync();
+
+                    return bandInfo.Select(b => new BandInfo()
+                    {
+                        ConnectionType = (int)b.ConnectionType,
+                        Name = b.Name
+                    }).ToList();
+                }));
+        }
+
+        public IAsyncOperation<bool> Connect(string name)
+        {
+            return AsyncInfo.Run<bool>((token) =>
+
+                 Task.Run<bool>(async () =>
+                 {
+                     var paired = await BandClientManager.Instance.GetBandsAsync();
+                     var band = paired.FirstOrDefault(x => x.Name == name);
+                     if (band != null)
+                     {
+                         StatusChanged?.Invoke(this, "connecting");
+                         this.connection = await BandClientManager.Instance.ConnectAsync(band);
+                         StatusChanged?.Invoke(this, "connected");
+                         connection.SensorManager.Accelerometer.ReadingChanged += (s, args) =>
+                         {
+                             if (AccelerometerChanged != null)
+                             {
+                                 AccelerometerChanged(this, new SensorReading()
+                                 {
+                                     X = args.SensorReading.AccelerationX,
+                                     Y = args.SensorReading.AccelerationY,
+                                     Z = args.SensorReading.AccelerationZ,
+                                     Timestamp = args.SensorReading.Timestamp
+                                 });
+                             }
+                         };
+                         await connection.SensorManager.Accelerometer.StartReadingsAsync();
+                     }
+                     return true;
+                 }));
+        }
+
+        public void Dispose()
+        {
+            if (connection != null)
+            {
+                connection.Dispose();
+            }
+        }
+
+    }
+}
